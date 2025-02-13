c756af8cf3c0bdb2b1f113bc546bfee7049cba6c
Lam Tieu <tieunhatlam1210@gmail.com>
2025-02-12 23:43:55 -0400

    Modify splash screen using custom fonts and background image

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -4,29 +4,43 @@ import ScreenWrapper from '../components/ScreenWrapper'
 import { StatusBar } from 'expo-status-bar'
 import { heightPer, widthPer } from '../helpers/common'
 import Button from '../components/Button'
+import { useFonts } from 'expo-font'
+import { ImageBackground } from 'react-native'
 
 const Welcome = () => {
+  const [fontsLoaded] = useFonts({
+    "Atures 700": require("../assets/fonts/Atures 700 PERSONAL USE ONLY.ttf"),
+    "Montserrat Alternates Medium": require("../assets/fonts/Montserrat Alternates Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading page...</Text>;
+  }
+
   return (
-    <ScreenWrapper bg="#141823">
+    <>
       <StatusBar style="light"></StatusBar>
-      <View style={styles.container}>
-        <Image style={styles.welcomeImage} resizeMode="contain" source={require("../assets/images/welcome img.png")}></Image>
+      <ImageBackground style={styles.backgroundImage} resizeMode="cover" source={require("../assets/images/splash img.png")}>
+        <View style={styles.container}>
+          <Image style={styles.welcomeImage} resizeMode="contain" source={require("../assets/images/welcome img.png")}></Image>
 
-        <View style={{ gap: 5 }}>
-          <Text style={styles.title}>AstroHub</Text>
-          <Text style={styles.punchline}>Explore. Connect. Go Beyond!</Text>
-        </View>
+          <View style={{ gap: 6 }}>
+            <Text style={styles.title}>AstroHub</Text>
+            <Text style={styles.punchline}>Explore. Connect. Go Beyond!</Text>
+          </View>
 
-        <View style={styles.footer}>
-          <Button 
-            title='Join the crew'
-            buttonStyle={{ marginHorizontal: widthPer(3) }}
-            textStyle={{color: "white"}}
-            onPress={() => {}}
-          />
+          <View style={styles.footer}>
+            <Button 
+              title='Join the crew'
+              buttonStyle={{ marginHorizontal: widthPer(3) }}
+              textStyle={{color: "white"}}
+              onPress={() => {}}
+            />
+          </View>
         </View>
-      </View>
-    </ScreenWrapper>
+      </ImageBackground>
+      
+    </>
   )
 }
 
@@ -34,21 +48,26 @@ export default Welcome
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: heightPer(4),
     flex: 1,
     alignItems: "center",
-    // justifyContent: "space-around",
-    backgroundColor: "#141823",
-    paddingHorizontal: widthPer(4)
+  },
+  backgroundImage: {
+    width: widthPer(100),
+    height: '100%'
   },
   welcomeImage: {
-    height: heightPer(40),
+    height: heightPer(50),
     width: widthPer(100),
     alignSelf: "center",
   },
   title: {
-    fontFamily: "Atures 700 PERSONAL USE ONLY",
+    fontFamily: "Atures 700",
     color: "white",
-    fontSize: heightPer(5),
+    fontSize: heightPer(5.5),
+    textShadowColor: 'rgba(255, 255, 255, 0.5)',
+    textShadowOffset: {width: 0, height: 5},
+    textShadowRadius: 1,
     textAlign: "center",
     letterSpacing: 5.76
   },
@@ -59,6 +78,6 @@ const styles = StyleSheet.create({
   },
   footer: {
     width: '50%',
-    paddingVertical: 120
+    paddingVertical: 100
   }
 })
