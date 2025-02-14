f31fd9f84d00d0e35f68696ff02e38c2167b625d
Lam Tieu <tieunhatlam1210@gmail.com>
2025-02-13 21:54:27 -0400

    Add a Loading component

---
+++ components/Loading.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
+import React from 'react'
+
+const Loading = ({size = 'large', color = '#FFFF'}) => {
+  return (
+    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size={size} color={color}></ActivityIndicator>
+    </View>
+  )
+}
+
+export default Loading
+
+const styles = StyleSheet.create({})
