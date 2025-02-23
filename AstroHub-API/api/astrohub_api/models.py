7e43aa117108d04ccdb684051731cc2380e86e4f
Lam Tieu <tieunhatlam1210@gmail.com>
2025-02-22 23:33:31 -0400

    Create CustomUser model

--- AstroHub-API/api/astrohub_api/models.py
+++ AstroHub-API/api/astrohub_api/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
 
+    def __str__(self):
+        return self.username
