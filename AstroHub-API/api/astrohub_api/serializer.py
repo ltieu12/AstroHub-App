95a003a9a5a89411c9547266cae127ce805e3aa4
Lam Tieu <tieunhatlam1210@gmail.com>
2025-02-22 23:36:08 -0400

    Add register serializer to create a user

--- AstroHub-API/api/astrohub_api/serializer.py
+++ AstroHub-API/api/astrohub_api/serializer.py
@@ -1,20 +1,19 @@
 from rest_framework import serializers
-from .models import CustomUser
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model=User
-        fields=['id', 'username', 'email', 'password', 'name']
+        model = User
+        fields = ['username', 'email', 'password', 'name']
 
         def create(self, validated_data):
             user = User.objects.create_user(
                 username=validated_data["username"],
                 email=validated_data["email"],
                 password=validated_data["password"],
-                name=validated_data.get("name", "")  # get the name field
+                name=validated_data.get("name", "")
             )
 
             return user
