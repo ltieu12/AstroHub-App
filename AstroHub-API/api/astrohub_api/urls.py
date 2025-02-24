f4281515a87ab676dbea1510a907d24c1f7baa78
Lam Tieu <tieunhatlam1210@gmail.com>
2025-02-23 23:31:35 -0400

    Add LoginView for login API endpoint

--- AstroHub-API/api/astrohub_api/urls.py
+++ AstroHub-API/api/astrohub_api/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login')
 ]
--- AstroHub-API/api/astrohub_api/views.py
+++ AstroHub-API/api/astrohub_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializer import RegisterSerializer
 
 class RegisterView(APIView):
@@ -10,3 +12,22 @@ class RegisterView(APIView):
             serializer.save()
             return Response({"message": "Registered user successfully!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            refresh_token = RefreshToken.for_user(user)
+
+            return Response({
+                "refresh": str(refresh_token),
+                "access": str(refresh_token.access_token),
+                "username": user.username,
+                "email": user.email
+            }, status=status.HTTP_200_OK)
+        
+        return Response({"Error: Invalid username or password!"}, status=status.HTTP_401_UNAUTHORIZED)
