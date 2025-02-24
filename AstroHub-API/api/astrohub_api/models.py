dd4a32dd896813914e0034380700ccbadee386be
Lam Tieu <tieunhatlam1210@gmail.com>
2025-02-23 23:30:36 -0400

    Add profile_pic and bio fields to User model

--- AstroHub-API/api/astrohub_api/models.py
+++ AstroHub-API/api/astrohub_api/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.username
