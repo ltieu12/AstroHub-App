b7de2b9ceb71afc9fca9b0b36a4786402151d376
Lam Tieu <tieunhatlam1210@gmail.com>
2025-02-26 21:48:39 -0400

    Create an endpoint for deleting user

--- AstroHub-API/api/astrohub_api/urls.py
+++ AstroHub-API/api/astrohub_api/urls.py
@@ -1,14 +1,9 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 from django.urls import path
-from .views import RegisterView, LoginView, UserListView
+from .views import RegisterView, LoginView, UserListView, DeleteUserView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('users/', UserListView.as_view(), name='users')
+    path('users/', UserListView.as_view(), name='users'),
+    path('delete-user/', DeleteUserView.as_view(), name='delete-user')
 ]
--- AstroHub-API/api/astrohub_api/views.py
+++ AstroHub-API/api/astrohub_api/views.py
@@ -1,12 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, generics
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializer import RegisterSerializer, UserListSerializer
 from .models import CustomUser
+
+User = get_user_model()
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
@@ -40,8 +42,16 @@ class LoginView(APIView):
         else:
             return Response({"Error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
         
-
+# Get all users
 class UserListView(generics.ListAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserListSerializer
     permission_classes = [AllowAny]
+
+
+# Delete a user (only if user is logged in)
+class DeleteUserView(APIView):
+    def delete(self, request):
+        user = request.user 
+        user.delete()
+        return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
