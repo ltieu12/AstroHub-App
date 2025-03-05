c79f127d2225481fcfcc8124dbf84709f90a77df
Lam Tieu <tieunhatlam1210@gmail.com>
2025-03-04 23:19:54 -0400

    Style Input component to include error

--- AstroHub-UI/components/Input.jsx
+++ AstroHub-UI/components/Input.jsx
@@ -6,13 +6,13 @@ const Input = (props) => {
   return (
     <View style={[styles.container, props.containerStyles && props.containerStyles]}>
       <TextInput
-        style={{flex: 1, color: '#FFFFFF'}}
+        style={styles.input}
         placeholderTextColor='#B9B9B9'
         ref={props.inputRef && props.inputRef}
         {...props}
       >
       </TextInput>
-      {props.error ? <Text style={{ color: "#EF4949", gap: 0 }}>{ props.error }</Text> : null}
+      {props.error ? <Text style={{ color: "#EF4949"}}>{ props.error }</Text> : null}
     </View>
   )
 }
@@ -21,11 +21,14 @@ export default Input
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        height: heightPer(6),
-        borderBottomWidth: 1,
-        borderBottomColor: '#B9B9B9',
-        alignItems: 'center',
-        justifyContent: 'center'
+      height: heightPer(6),
+      justifyContent: 'center',
+    },
+    input: {
+      flex: 1, 
+      color: '#FFFFFF',
+      marginBottom: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#B9B9B9'
     }
 })
